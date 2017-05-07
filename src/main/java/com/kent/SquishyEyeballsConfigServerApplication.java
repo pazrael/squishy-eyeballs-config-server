83f09defd8a83026c08b5d46033b4b15f0c4a45d
Kent Christensen <lpkchri1@comcast.net>
2017-05-07 12:34:25 -0600

    Added initial project files

--- src/main/java/com/kent/SquishyEyeballsConfigServerApplication.java
+++ src/main/java/com/kent/SquishyEyeballsConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.kent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SquishyEyeballsConfigServerApplication {
 
 	public static void main(String[] args) {
